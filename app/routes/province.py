54ab897f7f02f253c43362739f5724c58fb4ff98
Dan Chen <dan.chen01@edu.sait.ca>
2025-03-07 16:20:47 -0700

    fix typo

--- app/routes/province.py
+++ app/routes/province.py
@@ -9,19 +9,22 @@ from app.models.city import City
 from starlette import status
 
 
-router = APIRouter(tags=['provinces'], prefix="/provinces")
+router = APIRouter(tags=["provinces"], prefix="/provinces")
 db_dependence = Annotated[Session, Depends(get_db)]
 
-@router.get("/", response_model = List[ProvinceBase])
-async def get_provinces(db:db_dependence):
+
+@router.get("/", response_model=List[ProvinceBase])
+async def get_provinces(db: db_dependence):
     db_provinces = db.query(Province).all()
     return [ProvinceBase.from_orm(province) for province in db_provinces]
 
 
-@router.get("/{province_id}/cities", response_model= List[CityBase])
-async def get_cities_by_pronvice(db:db_dependence,province_id: int):
+@router.get("/{province_id}/cities", response_model=List[CityBase])
+async def get_cities_by_province(db: db_dependence, province_id: int):
     db_cities = db.query(City).filter(City.province_id == province_id).all()
     if not db_cities:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="No cities found for this province."
-     )
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No cities found for this province.",
+        )
     return [CityBase.from_orm(city) for city in db_cities]
