846e6551ca6c32f9dca27cc52d5a1e63720d363d
Dan Chen <dan.chen01@edu.sait.ca>
2025-04-09 18:55:59 -0600

    change notification title length to 200 chars

--- app/models/notification.py
+++ app/models/notification.py
@@ -7,7 +7,7 @@ class Notification(Base):
     __tablename__ = "notifications"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    title = Column(String(50), nullable=False)
+    title = Column(String(200), nullable=False)
     content = Column(String(2000), nullable=False)
     project_id = Column(Integer, ForeignKey("projects.id"), nullable=False)
     task_id = Column(Integer, ForeignKey("tasks.id"), nullable=False)
--- app/schemas/task.py
+++ app/schemas/task.py
@@ -22,7 +22,6 @@ class TaskWithChildren(TaskBase):
 class TaskCreate(BaseModel):
     parent_id: Optional[int] = Field(default=None)
     name: str = Field(..., max_length=50)
-    company_id: Optional[int] = 1
     sort_order: Optional[int] = 0
 
     class Config:
